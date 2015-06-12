9309d0700eb3317eb05aacffd2bf98d2a6b3f55d
mohamed <msamy@sufigeeks.org>
2015-06-12 18:39:41 -0400

    Need to fix performance and correct small numbers

--- TapeChallengeEclipse/src/Solution.java
+++ TapeChallengeEclipse/src/Solution.java
@@ -1,3 +1,7 @@
+import java.util.Random;
+
+
+
 /**
  * Created by mohamed on 6/10/15.
  */
@@ -6,10 +10,34 @@
 
 // you can use System.out.println for debugging purposes, e.g.
 // System.out.println("this is a debug message");
-import  java.math.*;
+
+
+
 class Solution
 {
 
+	public long totalArray = Long.MIN_VALUE;
+	private Boolean isFirstTime = true;
+	
+	public static void main(String[] Args)
+	{
+		int[] anArray = new int[99999];
+		Random randomGenerator = new Random();
+		//
+		for(int x = 0; x <99999 ; x++)
+		{
+		int randInt = randomGenerator.nextInt(100);
+		anArray[x] = randInt;
+		
+		}
+		
+		Solution s = new Solution();
+		long t1 = System.currentTimeMillis();
+		System.out.println(s.solution(anArray));
+		long t2 = System.currentTimeMillis();
+		System.out.println((t2 - t1)/1000 + " seconds");
+		System.out.println(s.totalArray);
+	}
 
     public int solution(int[] A)
     {
@@ -23,6 +51,10 @@ class Solution
         {
             if ( p(A,x) < diff) {
                 diff = p(A, x);
+                if (diff == 0)
+                {
+                	return diff;
+                }
 
             }
 
@@ -37,22 +69,45 @@ class Solution
         
         int totalBefore = 0;
         int totalAfter = 0;
-        for (int x = 0; x < A.length; x++)
+       
+        if(isFirstTime)
         {
-        	if (x <= P-1)
-        	{
-            totalBefore+= A[x];
-        	}
-        	else
-        	{
-        		totalAfter+= A[x];
-        	}
-        		
+
+       	 for (int x = 0; x < A.length; x++)
+            {
+            	if (x <= P-1)
+            	{
+                totalBefore+= A[x];
+            	}
+            	else
+            	{
+            		totalAfter+= A[x];
+            	}
+            		
+            }
         }
+        else
+        {
+        	
+        	for (int x = 0; x < P-1 ; x++)
+            {
+            	
+                totalBefore+= A[x];
+            	
+            	
+            }
+        	totalAfter = (int) (totalArray - totalBefore);
+        }
+        
+        
+       
 
         
-
-
+        if (totalArray == Long.MIN_VALUE)
+        {
+        	totalArray = (long) (totalBefore + totalAfter);
+        	isFirstTime = false;
+        }
         return  Math.abs(totalBefore - totalAfter);
     }
 }
